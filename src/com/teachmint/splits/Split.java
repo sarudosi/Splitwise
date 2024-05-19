1f0520810262d39b5bc24df49505bff8acd98186
sarudosi <43669635+sarudosi@users.noreply.github.com>
2024-05-19 17:21:35 +0530

    Update Split.java

--- src/com/teachmint/splits/Split.java
+++ src/com/teachmint/splits/Split.java
@@ -1,1 +1,49 @@
+package com.teachmint.splits;
+
+import com.teachmint.dao.User;
+
+public abstract class Split {
+	
+	final private SplitType splitType;
+    private User user;
+    private double amount;
+
+    public Split(SplitType splitType, User user, double amount) {
+        this.splitType = splitType;
+        this.user = user;
+        this.amount = amount;
+    }
+
+    public SplitType getSplitType() {
+        return splitType;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    abstract public double getShare();
+
+	@Override
+	public String toString() {
+		return "Split [splitType=" + splitType + ", user=" + user + ", amount=" + amount + "]";
+	}
+
+   
+    }
+
+
 
